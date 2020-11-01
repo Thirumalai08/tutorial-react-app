6697d4b7ea8fd9f17ab17108102013ee53f6e611
Thirumalai08 <thirunambi422@gmail.com>
2020-11-01 13:43:35 +0530

    initial commit

---
+++ src/Greetings/Greeting.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Greeting(){
+    // Date
+  let curDate = new Date()
+  curDate = curDate.getHours()
+  // Styling
+  const greetingStyle = {  }
+  let greeting = ''
+  /* Greeting
+  let greeting = curDate >= 1 && curDate < 12 ? 'Good Morning' : 
+                curDate >=12 && curDate < 16 ? 'Good Afternoon': 
+                curDate >= 16 && curDate < 19 ? 'Good Evening' : 'Good Night' */
+  if(curDate >= 1 && curDate < 12){
+    greeting = 'Good Morning'
+    greetingStyle.color = "green"
+  }else if(curDate >=12 && curDate < 16){
+    greeting = 'Good Afternoon'
+    greetingStyle.color = "yellow"
+  }else if(curDate >= 16 && curDate < 19){
+    greeting  = 'Good Evening'
+    greetingStyle.color = "blue"
+  }else{
+    greeting = 'Good Night'
+    greetingStyle.color = "#333"
+  }
+    return(
+        <div>
+            <h1>Hello React App, <span style={greetingStyle}>{greeting}</span></h1>
+        </div>
+    )
+}
+export default Greeting
