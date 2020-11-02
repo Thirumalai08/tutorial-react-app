95e32d923483c9bf773d1b631c61057959e3c6bf
Thirumalai08 <thirunambi422@gmail.com>
2020-11-02 10:27:05 +0530

    Counter done with state

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './counter.css'
 function Counter(){
+    // State to manage the count
+    const [count,setCount] = useState(0)
+    const handleCount = () => {
+        setCount(count + 1)
+        //console.log('Clicked')
+    }
     return(
         <div className="counter">
             <div className="counter-content">
-                <h4>0</h4>
-                <button>Click</button>
+                <h4>{count}</h4>
+                <button onClick={handleCount}>Click</button>
             </div>
         </div>
     )
